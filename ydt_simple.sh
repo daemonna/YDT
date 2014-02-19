15aeca534c3efc2bf8d0cdd5ed3d91111c9ca1d6
peter ducai <peter.ducai@gmail.com>
2014-02-20 00:21:26 +0100

    fixed toolchain downloads

--- ydt_simple.sh
+++ ydt_simple.sh
@@ -1,7 +1,38 @@
-#!/bin/bash
+#!/bin/bash  
 
-# simple Yocto devel toolkit script to install Yocto or it's toolchains
-# author: peter.ducai@gmail.com
+#
+# Author : peter.ducai@gmail.com 
+# Homepage : 
+# License : BSD http://en.wikipedia.org/wiki/BSD_license
+# Copyright 2014, peter ducai
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met: 
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer. 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution. 
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software without
+#    specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Purpose : Yocto development toolkit installer
+# Usage : run without paramaters to see usage
+#
 
 # HOST data
 HOST_ARCH=$(uname -m)
@@ -9,9 +40,9 @@ HOST_OS=$(uname -o)
 HOST_KERNEL_VERSION=$(uname -r)
 
 # TARGET
-TARGET_ARCH="i586"  #(i586 x86_64 powerpc mips armv7a armv5te)
+declare -a TARGET_ARCHS=("armv5te" "armv7a" "i586" "mips32" "ppc7400" "ppce500v2" "x86_64" )
 declare -a TARGETS=("qemumips" "qemuppc" "qemux86" "qemux86-64" "genericx86" "genericx86-64" "beagleboard" "mpc8315e-rdb" "routerstationpro")
-PACKAGE_MANAGER="rpm"
+declare -a PACKAGE_MANAGERS=("rpm" "tar" "deb" "ipk")
 IMAGE_RECIPE="core-image-sato" #default for toolchains
 TARGET="qemux86" #or MACHINE in config
 
@@ -44,12 +75,13 @@ install_full_yocto() {
   source ${YOCTO_DISTRO}/oe-init-build-env
 
   # change MACHINE in conf/local.conf
-  sed -i "s/MACHINE ??= \"qemux86\"/MACHINE ??= \"${TARGET}\"/g" conf/local.conf
+  sed -i "s/MACHINE ??= \"qemux86\"/MACHINE ??= \"${TARGETS[@]}\"/g" conf/local.conf
 
   echo "going to run bitbake (but sleeping 200s)"
+  sleep 400
   #run bitbake and build
-  bitbake -c fetchall ${IMAGE_TYPE}  #first just fetch all packages
-  bitbake ${IMAGE_TYPE}  #then build YOCTO
+  bitbake -c fetchall ${IMAGE_RECIPE}  #first just fetch all packages
+  bitbake ${IMAGE_RECIPE}  #then build YOCTO
 }
 
 ############################
@@ -57,14 +89,18 @@ install_full_yocto() {
 ############################
 install_toolchain_only() {
 
-  # download toolchain
-  echo -e "installing toolchain: ${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_TYPE}-${TARGET_ARCH}-toolchain-${YOCTO_VERSION}.sh"
-  wget ${YOCTO_REPO}/toolchain/${HOST_ARCH}/${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_TYPE}-${TARGET_ARCH}-toolchain-${YOCTO_VERSION}.sh
-  #run toolchain installer
+  for ta in "${TARGET_ARCHS[@]}"
+  do
+    # download toolchain
+    echo -e "DOWNLOADING toolchain from ${YOCTO_REPO}/toolchain/${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh"
+    #sleep 400
+    wget ${YOCTO_REPO}/toolchain/${HOST_ARCH}/${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh
+    #run toolchain installer
 
-  # and execute toolchain installer
-  echo -e "running toolchain"
-  sh ${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_TYPE}-${TARGET_ARCH}-toolchain-${YOCTO_VERSION}.sh
+    # and execute toolchain installer
+    echo -e "running toolchain ${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh"
+    #sh ${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh
+  done  
 }
 
 
@@ -78,7 +114,7 @@ collect_user_data() {
   if [[ ! -z ${INSTDIR} ]];then
     INSTALL_DIR=${INSTDIR}
   fi
-  echo "install dir set to ${INSTALL_DIR}"
+  
   
 #check if folder exists
   if [[ -d ${INSTALL_DIR} ]];then
@@ -90,26 +126,41 @@ collect_user_data() {
     cd ${INSTALL_DIR}
   fi
 
-  echo -e "choose target: qemumips qemuppc qemux86 qemux86-64 genericx86 genericx86-64 beagleboard"
+  echo -e "choose target [${TARGETS[@]}]:"
   read TRG
   if [[ ! -z ${TRG} ]];then
-    TARGET=${TRG}
+    read -a TARGETS <<<${TRG}
   fi
-  echo "target set to ${TARGET}"
+  
 
-  echo -e "choose package manager [rpm], but also [tar, deb, ipk] available:"
+  echo -e "choose target architecture [${TARGET_ARCHS[@]}]:"
+  read TRGA
+  if [[ ! -z ${TRGA} ]];then
+    read -a TARGET_ARCHS <<<${TRGA}
+  fi
+  
+
+  echo -e "choose package manager ${PACKAGE_MANAGERS[@]}:"
   read PKG
   if [[ ! -z ${PKG} ]];then
-    PACKAGE_MANAGER=${PKG}
+    read -a PACKAGE_MANAGERS <<<${PKG}
   fi
-  echo "package manager set to ${PACKAGE_MANAGER}"
+  
 
-  echo -e "choose image type [core-image-sato], but also [core-image-minimal, core-image-base, core-image-sato-dev, core-image-lsb]"
+  echo -e "choose image type ${IMAGE_RECIPE}:"
   read IMGTYPE
   if [[ ! -z ${IMGTYPE} ]];then
-    IMAGE_TYPE=${IMGTYPE}
+    IMAGE_RECIPE=${IMGTYPE}
   fi
-  echo "image type set to ${IMAGE_TYPE}"
+  
+
+  echo -e "\n\nSummary:"
+  echo "install dir set to ${INSTALL_DIR}"
+  echo "target set to ${TARGETS[@]}"
+  echo "target archs set to ${TARGET_ARCHS[@]}"
+  echo "package manager set to ${PACKAGE_MANAGERS}"
+  echo "image type set to ${IMAGE_RECIPE}"
+
 }
 
 
@@ -124,22 +175,16 @@ echo -e "##################################################"
 echo -e "do you want to proceed with:"
 echo -e "[1] full YOCTO install"
 echo -e "[2] toolchain only"
-echo ""
+echo "enter your choice:"
 read CHOICE
 
 
+collect_user_data
 
 case "${CHOICE}" in
-1) collect_user_data
-  install_full_yocto
+1) install_full_yocto
   ;;
-2) echo -e "image type is: ${IMAGE_TYPE}" 
-  echo -e "select target arch: i586 x86_64 powerpc mips armv7a armv5te"
-  read TRG
-  if [[ ! -z {$TRG} ]];then
-    TARGET=${TRG}
-  fi
-  install_toolchain_only
+2) install_toolchain_only
   ;;
 *) echo "wrong choice"
   ;;
