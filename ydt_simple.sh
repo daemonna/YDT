5c331bf93ab7ed0b834f45b9eabfd8c5d8d2ce02
peter ducai <peter.ducai@gmail.com>
2014-02-28 00:05:27 +0100

    fixes

--- ydt_simple.sh
+++ ydt_simple.sh
@@ -73,94 +73,222 @@ declare -a PACKAGE_MANAGERS=("rpm" "tar" "deb" "ipk")
 
 
 
+
+#########################################################################################
+#                                                                                       #
+# FUNCTIONS                                                                             #
+#########################################################################################
+
+############################
+# download STABLE branch   #
+############################
 get_stable_branch() {
-  echo -e "downloading STABLE"
-  wget ${YOCTO_REPO}/poky-dora-10.0.1.tar.bz2
-  tar xvjf poky-dora-10.0.1.tar.bz2
+
+    cd ${INSTALL_DIR}/${YOCTO_DISTRO}
+    echo -e "downloading STABLE branch"
+    wget ${YOCTO_REPO}/poky-dora-10.0.1.tar.bz2
+    tar xvjf poky-dora-10.0.1.tar.bz2 
 }
 
-get_current_branch() {
-  if [[ -d ${INSTALL_DIR}/${YOCTO_DISTRO} ]];then
-    if [[ -d ${INSTALL_DIR}/${YOCTO_DISTRO}/.git ]];then
-      echo -e "YOCTO git folder already exists.. updating with git."
-      git pull
-    else
-      echo -e "YOCTO folder already exist but it's not GIT repository. Please delete it and rerun installer."
-    fi
-  else
-    git clone git://git.yoctoproject.org/${YOCTO_DISTRO}
-  fi
+build_branch() {
+
+    echo -e "[build_branch] initialized"
+
+    # source directory to default 'build' dir
+    source ${YOCTO_DISTRO}/oe-init-build-env
+
+    #TODO check if multiple machines can be in config
+    # change MACHINE in conf/local.conf
+    sed -i "s/MACHINE ??= \"qemux86\"/MACHINE ??= \"${MACHINES[@]}\"/g" conf/local.conf
+
+    #run bitbake and build
+    echo -e "downloading IMAGES"
+    bitbake -c fetchall ${IMAGES[@]}  #first just fetch all packages
+    echo -e "RUNNING BITBAKE..................."
+    bitbake ${IMAGES[@]}  #then build YOCTO
 }
+
+build_sdk() {
+
+    echo -e "[build_sdk] initialized"    
+    
+    # source directory to default 'build' dir
+    source ${YOCTO_DISTRO}/oe-init-build-env
+
+    #TODO check if multiple machines can be in config
+    # change MACHINE in conf/local.conf
+    sed -i "s/MACHINE ??= \"qemux86\"/MACHINE ??= \"${MACHINES[@]}\"/g" conf/local.conf
+
+    # build sdk
+    echo -e "RUNNING BITBAKE..................."
+    bitbake -c do_populatesdk ${IMAGES[@]}  
+}
+
+
+#############################
+# download CURRENT branch   #
+#############################
+get_current_branch() {
+
+    echo -e "downloading CURRENT branch"
+    if [[ -d ${INSTALL_DIR}/${YOCTO_DISTRO} ]];then
+        echo "folder ${INSTALL_DIR}/${YOCTO_DISTRO} already exist!"
+        cd ${INSTALL_DIR}/${YOCTO_DISTRO}
+        
+        # if .git exist, then we got working CURRENT branch
+        if [[ -d ${INSTALL_DIR}/${YOCTO_DISTRO}/.git ]];then
+            echo -e "updating with git."
+            git pull
+        else
+            echo -e "YOCTO folder already exist but it's not GIT repository. Please delete it and rerun installer."
+            exit
+        fi
+        echo "ex"
+    else
+        cd ${INSTALL_DIR}
+        git clone git://git.yoctoproject.org/${YOCTO_DISTRO}
+    fi
+}
+
+
+############################################################
+# collect info from user about distro, machine and image   #
+############################################################
+collect_yocto_details() {
+
+    echo -e "processing Yocto details....................................."
+        
+    DISTROS=($(ls ${INSTALL_DIR}/${YOCTO_DISTRO}/meta*/conf/distro/*.conf| grep 'conf/distro/' | cut -d '/' -f 9 | cut -d '.' -f 1))
+    MACHINES=($(ls ${INSTALL_DIR}/${YOCTO_DISTRO}/meta*/conf/machine/*.conf| cut -d '/' -f 9 | cut -d '.' -f 1))
+    IMAGES=($(ls ${INSTALL_DIR}/${YOCTO_DISTRO}/meta*/recipe*/images/*.bb |cut -d '/' -f 9 | cut -d '.' -f 1))
+    
+    echo -e "collecting user data........................................."
+    echo -e "............................................................."
+
+    # get info about DISTRO
+    echo -e "Which DISTRO you want to use?"
+    echo -e "[${DISTROS[@]}]:"
+
+    read DST  
+
+    if [[ -z "${DST}" ]];then
+        #echo "using orig. value: ${DISTROS[@]}"
+        echo "."
+    else
+        DISTROS="${DST}"        
+    fi
+    echo "using ${DISTROS[@]}"
+    echo -e "............................................................."
+    
+    
+    # get info about MACHINE
+    echo -e "Which MACHINE you want to use?"
+    echo -e "[${MACHINES[@]}]:"
+
+    read MCH  
+
+    if [[ -z "${MCH}" ]];then
+        #echo "using orig. value: ${MACHINES[@]}"
+        echo "."
+    else
+        MACHINES="${MCH}"        
+    fi
+    echo "using ${MACHINES[@]}"
+    echo -e "............................................................."
+    
+    
+    # get info about MACHINE
+    echo -e "Which IMAGE you want to use?"
+    echo -e "[${IMAGES[@]}]:"
+
+    read IMG  
+
+    if [[ -z "${IMG}" ]];then
+        #echo "using orig. value: ${IMAGES[@]}"
+        echo "."
+    else
+        IMAGES="${IMG}"        
+    fi
+    echo "using ${IMAGES[@]}"
+    echo -e "............................................................."
+    
+    echo -e "END OF PROCESSING"
+}
+
+
 
 ############################
 # collect info from user   #
 ############################
 collect_user_data() {
 
-  echo -e "collecting user data........................................."
-  echo -e "............................................................."
+    echo -e "collecting user data........................................."
+    echo -e "............................................................."
 
-  # CPU THREADS setting, if user set more threads than CPU have, warning is issued and script exits
-  echo -e "Your machine has ${CPU_THREADS} cores/thread. How many of them you want to use for building?"
-  printf "[${CPU_THREADS}]:"
+    # CPU THREADS setting, if user set more threads than CPU have, warning is issued and script exits
+    echo -e "Your machine has ${CPU_THREADS} cores/thread. How many of them you want to use for building?"
+    printf "[${CPU_THREADS}]:"
 
-  read THR
-  echo "want to set $THR threads"
+    read THR  
 
-  if [[ ! -z "${THR}" ]];then
-    if [[ "${THR}" > "${CPU_THREADS}" ]];then
-      echo -e "INVALID NUMBER! You try to assign ${THR} of ${CPU_THREADS} available!"
-      exit
+    if [[ -z "${THR}" ]];then
+        #echo "using orig. value: ${CPU_THREADS}"
+        echo "."
+    else
+        #echo "want to set $THR threads"
+        if [[ $((${THR})) > $((${CPU_THREADS})) ]];then
+            echo -e "INVALID NUMBER! You try to assign ${THR} of ${CPU_THREADS} available!"
+            exit
+        fi
+        CPU_THREADS="${THR}"        
     fi
-    CPU_THREADS="${THR}"
-  fi
-  echo -e "............................................................."
+    echo "using ${CPU_THREADS}"
+    echo -e "............................................................."
 
 
-  # set INSTALL folder
-  echo -e "Where do you want to install Yocto?"
-  printf "[${INSTALL_DIR}]:"
-  read INST
-  if [[ -z "${INST}" ]];then
-    echo "${INST} is set"
-    INSTALL_DIR="${INST}"
-  else
-    echo "value is empty"
-  fi
-  echo -e "switching to ${INSTALL_DIR}"
-  cd ${INSTALL_DIR}
-  echo -e "............................................................."
+    # path where Yocto will be installed
+    echo -e "Where you'd like to install Yocto or it's tools?"
+    printf "[${INSTALL_DIR}]:"
 
-  # set and download Yocto
-  echo -e "Do you want to use CURRENT (git) unstable or rather STABLE (wget) version?"
-  printf "[CURRENT]:"
-  read ${CUR}
-  if [[ ! -z "${CUR}" ]];then
-    if [[ "${CUR}" == "STABLE" ]];then
-      get_stable_branch
-    else 
-      get_current_branch
+    read INSTD  
+
+    if [[ -z "${INSTD}" ]];then
+        #echo "using orig. value: ${INSTALL_DIR}"
+        echo "."
+    else
+        INSTALL_DIR="${INSTD}"        
     fi
-  fi
-  echo -e "............................................................."
+    echo "using ${INSTALL_DIR}"
+    mkdir ${INSTALL_DIR}
+    echo -e "............................................................."
+    
+    
+    
+    # choice between cutting-edge and stable
+    echo -e "Do you want to use CURRENT (git) unstable or rather STABLE (wget) version?"
+    printf "[CURRENT]:"
 
-  echo -e "inspecting downloaded files"
-  DISTROS=($(ls ${INSTALL_DIR}/meta*/conf/distro/*.conf| grep 'conf/distro/' | cut -d '/' -f 9 | cut -d '.' -f 1))
-  MACHINES=($(ls ${INSTALL_DIR}/meta*/conf/machine/*.conf| cut -d '/' -f 9 | cut -d '.' -f 1))
-  IMAGES=($(ls ${INSTALL_DIR}/meta*/recipe*/images/*.bb |cut -d '/' -f 9 | cut -d '.' -f 1))
+    read CUST  
 
- 
-
-
+    if [[ -z "${CUST}" ]];then
+        #echo "using orig. value: ${INSTALL_DIR}"
+        echo "."
+    else
+        INSTALL_DIR="${INSTD}"        
+    fi
+    
+    if [[ -z "${CUST}" ]];then
+        get_current_branch
+    else
+        echo "not empty: ${CUST}"
+        if [[ ${CUST} == "STABLE" ]];then
+            get_stable_branch
+        else
+            echo "wrong choice"
+        fi
+    fi
   
-
-  echo -e "\n\nSummary:"
-  echo "install dir set to ${INSTALL_DIR}"
-  echo "target set to ${TARGETS[@]}"
-  echo "target MACHINE set to ${MACHINES[@]}"
-  echo "package manager set to ${PACKAGE_MANAGERS}"
-  echo "image type set to ${IMAGES[@]}"
-  sleep 40000
+    echo -e "............................................................."
 }
 
 ###############################
@@ -226,19 +354,18 @@ install_full_yocto_devel() {
 # toolchain installer      #
 ############################
 install_toolchain_only() {
+    for ta in "${TARGET_ARCHS[@]}"
+    do
+        # download toolchain
+        echo -e "DOWNLOADING toolchain from ${YOCTO_REPO}/toolchain/${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh"
+        #sleep 400
+        wget ${YOCTO_REPO}/toolchain/${HOST_ARCH}/${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh
+        #run toolchain installer
 
-  for ta in "${TARGET_ARCHS[@]}"
-  do
-    # download toolchain
-    echo -e "DOWNLOADING toolchain from ${YOCTO_REPO}/toolchain/${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh"
-    #sleep 400
-    wget ${YOCTO_REPO}/toolchain/${HOST_ARCH}/${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh
-    #run toolchain installer
-
-    # and execute toolchain installer
-    echo -e "running toolchain ${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh"
-    #sh ${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh
-  done  
+        # and execute toolchain installer
+        echo -e "running toolchain ${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh"
+        #sh ${YOCTO_DISTRO}-eglibc-${HOST_ARCH}-${IMAGE_RECIPE}-${ta}-toolchain-${YOCTO_VERSION}.sh
+    done  
 }
 
 
@@ -253,23 +380,7 @@ install_toolchain_only() {
 echo -e "welcom to simple YOCTO installer"
 echo -e "##################################################"
 
-#echo -e "do you want to proceed with:"
-#echo -e "[1] full Poky 10.0.1 install (stable)"
-#echo -e "[2] full Poky install (cutting edge)"
-#echo -e "[3] toolchain only"
-#echo "enter your choice:"
-#read CHOICE
-
-
 collect_user_data
+collect_yocto_details
 
-#case "${CHOICE}" in
-#1) install_full_yocto
-#  ;;
-#2) install_full_yocto_devel
-#  ;;
-#3) install_toolchain_only
-#  ;;
-#*) echo "wrong choice"
-  ;;
-esac
+exit $?
